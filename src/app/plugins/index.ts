8f32d0d7d6dd1dc13fac3f2b63f58eec6ed58731
Papuzzitas-97 <papuzzitas-97@hotmail.it>
2026-02-02 23:20:00 +0100

    bug versione produzione

--- src/app/plugins/index.ts
+++ src/app/plugins/index.ts
@@ -1,10 +1,10 @@
 
 import {ActionsManagerComponent} from "./actions-manager/actions-manager.component";
+import {FrancolinoComponent} from "./francolino/francolino.component";
 import {FsmComponent} from "./fsm/fsm.component";
+import {LlmChatboxComponent} from "./llm-chatbox/llm-chatbox.component";
 import {RobotCamViewerComponent} from "./robot-cam-viewer/robot-cam-viewer.component";
 import {RobotSpeechComponent} from "./robot-speech/robot-speech.component";
 import {ServicesManagerComponent} from "./services-manager/services-manager.component";
-import {LlmChatboxComponent} from "./llm-chatbox/llm-chatbox.component";
-import {FrancolinoComponent} from "./francolino/francolino.component";
 
-export const pluginIndex = { "Actions Manager": ActionsManagerComponent, "Finite State Machine": FsmComponent, "Robot Cam Viewer": RobotCamViewerComponent, "Robot Speech": RobotSpeechComponent, "Services Manager": ServicesManagerComponent, "LLM Chatbox": LlmChatboxComponent, "Francolino": FrancolinoComponent };
+export const pluginIndex = { "Actions Manager": ActionsManagerComponent, "Francolino": FrancolinoComponent, "Finite State Machine": FsmComponent, "llm-chatbox": LlmChatboxComponent, "Robot Cam Viewer": RobotCamViewerComponent, "Robot Speech": RobotSpeechComponent, "Services Manager": ServicesManagerComponent }
